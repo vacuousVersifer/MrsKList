942b4658f85c59c7bd0e5246fd0e861a0b9c9410
Glitch (mrs-k-list) <none>
2021-03-31 18:05:56 +0000

    🏌⏰ Checkpoint ./modules/user.js:28018289/76 ./views/register.html:28018289/3041 ./views/login.html:28018289/1171 ./public/register.js:28018289/3961 ./public/login.js:28018289/2022 ./public/connect.js:28018289/68 ./data/users.json:28018289/556

--- modules/user.js
+++ modules/user.js
@@ -17,5 +17,7 @@ module.exports = {
     };
     
     users.addItem(newUser);
+    
+    console.log(["New user - user.js", newUser, users]);
   }
 };
---
+++ public/register.js
@@ -0,0 +1,85 @@
+/* globals io */
+
+// When the document is loaded
+$(document).ready(() => {
+  const socket = io();
+  
+  // Register elements
+  const registerForm = $("#registerForm");
+  const registerName = $("#registerName");
+  const registerCode = $("#registerCode");
+  const registerPassword = $("#registerPassword");
+  const registerPasswordConfirm = $("#registerPasswordConfirm");
+
+  let submittedCredentials;
+  registerForm.submit(e => {
+    e.preventDefault();
+
+    $("#registerError").text("");
+
+    let code = registerCode.val();
+    let password = registerPassword.val();
+    let passwordConfirm = registerPasswordConfirm.val();
+    let name = registerName.val();
+
+    let codeValid = true;
+    let passwordValid = password == passwordConfirm;
+    let nameValid = !name.length < 1;
+
+    if (!nameValid) {
+      $("#registerError").text("You must enter a value for your name");
+    }
+
+    if (!(password && passwordConfirm)) {
+      $("#registerError").text("You must enter values for your passwords");
+    }
+
+    if (!passwordValid) {
+      $("#registerError").text("Your passwords do not match");
+    }
+
+    for (let i = 0; i < code.length; i++) {
+      if (!Number.isInteger(parseInt(code[i]))) codeValid = false;
+    }
+
+    if (!codeValid) {
+      $("#registerError").text(
+        "Your code should be a continuous string of numbers, with no spaces"
+      );
+    }
+
+    if (code.length < 1) {
+      codeValid = false;
+      $("#registerError").text("You need to enter something for your code");
+    }
+
+    if (codeValid && passwordValid && nameValid) {
+      let newCredentials = {
+        code,
+        password,
+        name
+      };
+
+      submittedCredentials = {
+        code,
+        password
+      };
+
+      registerCode.val("");
+      registerPassword.val("");
+      registerPasswordConfirm.val("");
+      registerName.val("");
+
+      socket.emit("register user", newCredentials);
+    }
+  });
+
+  socket.on("register user completed", () => {
+    sessionStorage.setItem("destination", "login");
+    window.location.replace("/login");
+  });
+
+  socket.on("register user code taken", () => {
+    $("#registerError").text("That code is already taken!");
+  });
+});

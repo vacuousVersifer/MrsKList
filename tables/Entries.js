e77aac21e5b58f5891aeeb11d54b89ce234d97b0
vacuousVersifer <davisbruce413@gmail.com>
2021-06-11 16:31:17 -0500

    Adding stuff

--- tables/Entries.js
+++ tables/Entries.js
@@ -1,6 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define("entries", {
     name: DataTypes.STRING,
-    watched: DataTypes.STRING
+    watched: DataTypes.STRING,
+    must: DataTypes.BOOLEAN,
+    funny: DataTypes.BOOLEAN,
+    commit: DataTypes.BOOLEAN,
+    scary: DataTypes.BOOLEAN,
+    adult: DataTypes.BOOLEAN,
+    romance: DataTypes.BOOLEAN
   }, { timestamps: false })
 };
